02bad80af5056134376b8d853127d6217f8a91bc
Vee-keme <veliaprecious@gmail.com>
2023-11-07 03:15:22 +0100

    dashboard static header cards

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+// import TopCards from "./TopCards";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-4">
       <h2>Dashboard</h2>
       <h2>Welcome back, Precious</h2>
+      {/* <TopCards /> */}
     </div>
   );
 };
