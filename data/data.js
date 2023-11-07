1cd0a17c3e10e3e1c2a6f9d96e7456ec1a19f571
Vee-keme <veliaprecious@gmail.com>
2023-11-07 20:01:04 +0100

    recent orders component

---
+++ data/data.js
@@ -0,0 +1,113 @@
+const data = [
+  {
+    id: 1,
+    name: {
+      first: "john",
+      last: "doe",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 minutes ago",
+  },
+  {
+    id: 2,
+    name: {
+      first: "john",
+      last: "doe",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 minutes ago",
+  },
+  {
+    id: 3,
+    name: {
+      first: "john",
+      last: "doe",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 minutes ago",
+  },
+  {
+    id: 4,
+    name: {
+      first: "john",
+      last: "doe",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 minutes ago",
+  },
+  {
+    id: 5,
+    name: {
+      first: "john",
+      last: "doe",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 minutes ago",
+  },
+  {
+    id: 6,
+    name: {
+      first: "john",
+      last: "doe",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 minutes ago",
+  },
+  {
+    id: 7,
+    name: {
+      first: "john",
+      last: "doe",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 minutes ago",
+  },
+  {
+    id: 8,
+    name: {
+      first: "john",
+      last: "doe",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 minutes ago",
+  },
+  {
+    id: 9,
+    name: {
+      first: "john",
+      last: "doe",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 minutes ago",
+  },
+  {
+    id: 10,
+    name: {
+      first: "john",
+      last: "doe",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 minutes ago",
+  },
+];
+export default data;
